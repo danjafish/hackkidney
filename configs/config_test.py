336ff225ca4e8870c67176f81af982d12377d3a1
Daniil Fishman <d.fishman@lcgroup.su>
2020-12-24 20:18:40 +0300

    add config tests

---
+++ configs/config_test.py
@@ -0,0 +1,25 @@
+gpu_number = 2
+size_after_reshape = 320
+cross_val = False
+use_sampler = True
+use_adaptive_sampler = False
+size = 1024
+bs = 64
+epochs = 40
+epochs_minlr = 0
+not_empty_ratio = 0.5
+val_index = 7
+loss_name = 'comboloss'
+max_lr = 7e-4
+min_lr = 3e-6
+prefix = 'rnet34_combo'
+weights = {"bce": 1, "dice": 0, "focal": 0}
+s = 'w_'
+for weight in weights:
+    s+=str(weights[weight])
+    s+='-'
+if cross_val:
+    model_name = f'resize_cv_{s}_{size}_{bs}_{epochs}'
+else:
+    model_name = f'{prefix}_{s}_{size}_{size_after_reshape}_{bs}_{epochs}_{val_index}'
+step_size = int(size*0.5)
